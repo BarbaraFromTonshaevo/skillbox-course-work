2f4b6708a95b95213da3f0772e5c84cda763b757
Elkina Barbara <elkinabarbara1999@gmail.com>
2021-02-18 22:33:45 +0300

    without right gallary

--- script/script.js
+++ script/script.js
@@ -79,4 +79,272 @@ window.addEventListener('DOMContentLoaded', function(){
         })
     })
 
+    const max_images_6 = 6
+    const max_block_6 = 5
+
+    const max_images_4 = 4
+    const max_block_4 = 14 
+
+    var curr_block_6 = 1
+    var curr_block_4 = 1
+
+    var curr_img_6 = 6
+    var curr_img_4 = 4 
+
+    var img_src = './img/Gallary-'
+
+
+    document.querySelector('.right-slider-6').addEventListener('click', function(){
+        if(curr_block_6 < max_block_6 ){
+            document.querySelectorAll('.picture-item-6').forEach(function(ImgItem){
+                curr_img_6++
+                var full_src =  img_src + curr_img_6 + '.jpg'
+                var full_url = 'url(' + full_src + ')'
+                ImgItem.style.backgroundImage = full_url
+            })
+            curr_block_6++
+            var text_content = curr_block_6 + '/5'
+            // console.log(curr_block_6)
+            document.querySelector('.num-slider-6').textContent = text_content
+        }
+    })
+    document.querySelector('.left-slider-6').addEventListener('click', function(){
+        if(curr_block_6 > 1){
+            curr_img_6 = curr_img_6 - max_images_6*2
+            // console.log(curr_img_6)
+            document.querySelectorAll('.picture-item-6').forEach(function(ImgItem){
+                curr_img_6++
+                var full_src =  img_src + curr_img_6 + '.jpg'
+                var full_url = 'url(' + full_src + ')'
+                ImgItem.style.backgroundImage = full_url
+            })
+            curr_block_6--
+            var text_content = curr_block_6 + '/5'
+            // console.log(curr_block_6)
+            document.querySelector('.num-slider-6').textContent = text_content
+        }
+    })
+    document.querySelector('.right-slider-4').addEventListener('click', function(){
+        if(curr_block_4 < max_block_4 ){
+            curr_img_4 = curr_img_4 - 2
+            document.querySelectorAll('.picture-item-4').forEach(function(ImgItem){
+                curr_img_4++
+                var full_src =  img_src + curr_img_4 + '.jpg'
+                var full_url = 'url(' + full_src + ')'
+                ImgItem.style.backgroundImage = full_url
+            })
+            curr_block_4++
+            var text_content = curr_block_4 + '/14'
+            // console.log(curr_block_4)
+            document.querySelector('.num-slider-4').textContent = text_content
+        }
+    })
+    document.querySelector('.left-slider-4').addEventListener('click', function(){
+        if(curr_block_4 > 1 ){
+            curr_img_4 = curr_img_4 - 6
+            document.querySelectorAll('.picture-item-4').forEach(function(ImgItem){
+                curr_img_4++
+                var full_src =  img_src + curr_img_4 + '.jpg'
+                var full_url = 'url(' + full_src + ')'
+                ImgItem.style.backgroundImage = full_url
+            })
+            curr_block_4--
+            var text_content = curr_block_4 + '/14'
+            console.log(curr_block_4)
+            document.querySelector('.num-slider-4').textContent = text_content
+        }
+    })
+    // Accordion
+    document.querySelectorAll('.accordion-heading').forEach(function(AcrdBtn){
+        // console.log(AcrdBtn)
+        AcrdBtn.addEventListener('click',function(HeadBtn){
+            // console.log("CLICK")
+            var head = HeadBtn.currentTarget.dataset.head
+            document.querySelector(`[data-info="${head}"]`).classList.toggle('is-open')
+            document.querySelector(`[data-head="${head}"]`).classList.toggle('is-open')
+        })
+    })
+    document.querySelectorAll('.artists-item').forEach(function(PntrBtn){
+        PntrBtn.addEventListener('click',function(NameBtn){
+            console.log("CLICK")
+            var name = NameBtn.currentTarget.dataset.name
+            document.querySelectorAll('.info-block').forEach(function(TabContent){
+                TabContent.classList.remove('is-active')
+            })
+            document.querySelectorAll('.artists-item').forEach(function(TabContent){
+                TabContent.classList.remove('is-active')
+            })
+            document.querySelector(`[data-name="${name}"]`).classList.add('is-active')
+            document.querySelector(`[data-painter="${name}"]`).classList.add('is-active')
+        })
+    })
+
+    // Partner
+
+    var curr_part = 1
+    document.querySelector('.partner-slider-right').addEventListener('click',function(){
+        curr_part++
+        if(curr_part < 8){
+            var curr_part_img = 0
+            console.log("RIGHT")
+            document.querySelectorAll('.partner').forEach(function(TabPtnr){
+                var ptnr_src = 'url('+'./img/Group-'+ (curr_part+curr_part_img)+'.png)'
+                console.log(ptnr_src)
+                TabPtnr.style.backgroundImage = ptnr_src
+                curr_part_img++
+            })
+        }
+        else if(curr_part == 8){
+            document.querySelector('.partner-1').style.backgroundImage = 'url(./img/Group-8.png)'
+            document.querySelector('.partner-2').style.backgroundImage = 'url(./img/Group-9.png)'
+            document.querySelector('.partner-3').style.backgroundImage = 'url(./img/Group-1.png)'
+        }
+        else{
+            document.querySelector('.partner-1').style.backgroundImage = 'url(./img/Group-9.png)'
+            document.querySelector('.partner-2').style.backgroundImage = 'url(./img/Group-1.png)'
+            document.querySelector('.partner-3').style.backgroundImage = 'url(./img/Group-2.png)'
+            curr_part = 0
+        }
+
+
+    })
+    document.querySelector('.partner-slider-left').addEventListener('click',function(){
+        curr_part--
+        if(curr_part > 0){
+            var curr_part_img = 0
+            console.log("LEFT")
+            document.querySelectorAll('.partner').forEach(function(TabPtnr){
+                var ptnr_src = 'url('+'./img/Group-'+ (curr_part+curr_part_img)+'.png)'
+                console.log(ptnr_src)
+                TabPtnr.style.backgroundImage = ptnr_src
+                curr_part_img++
+            })
+        }
+        else if(curr_part == -1){
+
+            document.querySelector('.partner-1').style.backgroundImage = 'url(./img/Group-8.png)'
+            document.querySelector('.partner-2').style.backgroundImage = 'url(./img/Group-9.png)'
+            document.querySelector('.partner-3').style.backgroundImage = 'url(./img/Group-1.png)'
+            curr_part = 8
+        }
+        else{
+            document.querySelector('.partner-1').style.backgroundImage = 'url(./img/Group-9.png)'
+            document.querySelector('.partner-2').style.backgroundImage = 'url(./img/Group-1.png)'
+            document.querySelector('.partner-3').style.backgroundImage = 'url(./img/Group-2.png)'
+        }
+    })
+    
+    // Edition
+    var curr_ed = 1
+    document.querySelector('.right-slider-ed').addEventListener('click',function(){
+        if((curr_ed < 5)&&(document.body.clientWidth > 1920)){
+            curr_ed++
+            var curr_ed_item = 1
+            document.querySelectorAll('.editions-item').forEach(function(EdItem){
+                if((curr_ed == curr_ed_item)||(curr_ed+1 == curr_ed_item)||(curr_ed+2 == curr_ed_item)){
+                    EdItem.style.display = "block"
+                    curr_ed_item++
+                }
+                else{
+                    EdItem.style.display = "none"
+                    curr_ed_item++
+                }
+            })
+            var txtcnt = curr_ed + "/5"
+            document.querySelector('.num-slider-ed').textContent = txtcnt
+        }
+        if((curr_ed < 6)&&(document.body.clientWidth < 1920)){
+            curr_ed++
+            var curr_ed_item = 1
+            document.querySelectorAll('.editions-item').forEach(function(EdItem){
+                if((curr_ed == curr_ed_item)||(curr_ed+1 == curr_ed_item)){
+                    EdItem.style.display = "block"
+                    curr_ed_item++
+                }
+                else{
+                    EdItem.style.display = "none"
+                    curr_ed_item++
+                }
+            })
+            var txtcnt = curr_ed + "/6"
+            document.querySelector('.num-slider-ed').textContent = txtcnt
+        }
+    })
+    document.querySelector('.left-slider-ed').addEventListener('click',function(){
+        if((curr_ed > 1)&&(document.body.clientWidth > 1920)){
+            curr_ed--
+            var curr_ed_item = 1
+            document.querySelectorAll('.editions-item').forEach(function(EdItem){
+                if((curr_ed == curr_ed_item)||(curr_ed+1 == curr_ed_item)||(curr_ed+2 == curr_ed_item)){
+                    EdItem.style.display = "block"
+                    curr_ed_item++
+                }
+                else{
+                    EdItem.style.display = "none"
+                    curr_ed_item++
+                }
+            })
+            var txtcnt = curr_ed + "/5"
+            document.querySelector('.num-slider-ed').textContent = txtcnt
+        }
+        if((curr_ed > 1)&&(document.body.clientWidth < 1920)){
+            curr_ed--
+            var curr_ed_item = 1
+            document.querySelectorAll('.editions-item').forEach(function(EdItem){
+                if((curr_ed == curr_ed_item)||(curr_ed+1 == curr_ed_item)){
+                    EdItem.style.display = "block"
+                    curr_ed_item++
+                }
+                else{
+                    EdItem.style.display = "none"
+                    curr_ed_item++
+                }
+            })
+            var txtcnt = curr_ed + "/6"
+            document.querySelector('.num-slider-ed').textContent = txtcnt
+        }
+    })
+    // Здесь исправляется кол-во блоков при изменении ширины окна
+    window.addEventListener('resize',function(){
+        if(document.body.clientWidth > 1920 ){
+            if(curr_ed == 6){
+                curr_ed = 5
+            }
+            var src_1 = '.editions-item-'+(curr_ed)
+            var src_2 = '.editions-item-'+(curr_ed+1)
+            var src_3 = '.editions-item-'+(curr_ed+2)
+            document.querySelector(src_1).style.display = "block"
+            document.querySelector(src_2).style.display = "block"
+            document.querySelector(src_3).style.display = "block"
+            var txtcnt = curr_ed + "/5"
+            document.querySelector('.num-slider-ed').textContent = txtcnt
+
+        }
+        else if(document.body.clientWidth < 768){
+            document.querySelectorAll('.editions-item').forEach(function(EdItem){
+                EdItem.style.display = "block"
+            })
+        }
+        else{
+            var curr_ed_item = 1
+            document.querySelectorAll('.editions-item').forEach(function(EdItem){
+                if((curr_ed == curr_ed_item)||(curr_ed+1 == curr_ed_item)){
+                    EdItem.style.display = "block"
+                    curr_ed_item++
+                }
+                else{
+                    EdItem.style.display = "none"
+                    curr_ed_item++
+                }
+            })
+            var txtcnt = curr_ed + "/6"
+            document.querySelector('.num-slider-ed').textContent = txtcnt
+        }
+    })
+    // Tooltip
+    document.querySelectorAll('.tooltip').forEach(function(TltpBtn){
+        TltpBtn.addEventListener('click',function(){
+            TltpBtn.classList.toggle('is-open')
+        })
+    })
 })
